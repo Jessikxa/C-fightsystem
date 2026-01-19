19f0bd6690a88fea079ce8434652626d3c0515fb
Jessikxa <iamjessicadurkin@gmail.com>
2026-01-19 14:31:12 +0100

    adding stats

--- fightsystem c++/fightsystem c++.cpp
+++ fightsystem c++/fightsystem c++.cpp
@@ -3,11 +3,65 @@
 
 #include <iostream>
 
+class player
+{
+	public:
+		void stats() //defines player stats
+		{
+			int health = 100;
+			int mana = 100;
+			int stamina = 100;
+			int strength = 20;
+			int intelligence = 25;
+			int agility = 15;
+		}
+};
+
+class enemy
+{
+	public:
+		void stats() //defines enemy stats
+		{
+			int health = 80;
+			int mana = 50;
+			int stamina = 70;
+			int strength = 15;
+			int intelligence = 10;
+			int agility = 20;
+		}
+};
+
+class abilities
+{
+	public:
+		void stats() //defines player abilities
+		{
+			int fireball = 20;
+			int iceSpike = 25;
+			int shadow = 30;
+			int lightningStrike = 35;
+			int waterblast = 40;
+			int orbOfDestruction = 50;
+			int earthquake = 45;
+			int tornado = 35;
+			int heal = 15;
+		}
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
 
- 
+	player player1;
+	enemy dragon(); // see how can give certain stats to certain enemies later
+	abilities playerAbilities;
+	abilities enemyAbilities;
+
+	//display player stats when fighting opdiotn is selected
+	player1.stats();
+	
+
+
     
     
 	//is this where i can put the fight system code, or seperate class?
@@ -17,13 +71,4 @@ int main()
 	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
